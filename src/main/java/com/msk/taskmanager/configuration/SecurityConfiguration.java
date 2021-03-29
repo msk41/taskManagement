adf5582a3c207ffe9b35c6e50a1583fe6c31b7c0
masakihigashi <masaki.h.5555@gmail.com>
2021-03-29 21:25:25 +0900

    Enable access to h2 console

--- src/main/java/com/msk/taskmanager/configuration/SecurityConfiguration.java
+++ src/main/java/com/msk/taskmanager/configuration/SecurityConfiguration.java
@@ -10,5 +10,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 }
