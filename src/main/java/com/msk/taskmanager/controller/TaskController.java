623ac1fea088fa6f7717dc25071f0875967d94a1
masakihigashi <masaki.h.5555@gmail.com>
2021-03-31 22:07:50 +0900

    Add methods to create tasks

--- src/main/java/com/msk/taskmanager/controller/TaskController.java
+++ src/main/java/com/msk/taskmanager/controller/TaskController.java
@@ -1,20 +1,29 @@
 package com.msk.taskmanager.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.msk.taskmanager.model.Task;
+import com.msk.taskmanager.model.User;
 import com.msk.taskmanager.service.TaskService;
+import com.msk.taskmanager.service.UserService;
 
 @Controller
 public class TaskController {
 
     private TaskService taskService;
+    private UserService userService;
 
     @Autowired
-    public TaskController(TaskService taskService) {
+    public TaskController(TaskService taskService, UserService userService) {
         this.taskService = taskService;
+        this.userService = userService;
     }
 
     @GetMapping("/tasks")
@@ -22,5 +31,25 @@ public class TaskController {
 
         model.addAttribute("tasks", taskService.findAll());
         return "views/tasksList";
+    }
+
+    @GetMapping("task/create")
+    public String showTaskForm(Model model) {
+
+        User user = userService.getUserByEmail("admin@mail.com");
+        Task task = new Task();
+        task.setCreatorName(user.getName());
+        model.addAttribute("task", task);
+        return "views/emptyTaskForm";
+    }
+
+    @PostMapping("task/create")
+    public String createTask(@Valid Task task, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "views/emptyTaskForm";
+        }
+        taskService.createTask(task);
+
+        return "redirect:/tasks";
     }
 }
