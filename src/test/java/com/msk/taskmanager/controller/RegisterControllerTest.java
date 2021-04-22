7e9d4a9f2f3d18fa8dc1903fad9cb8995be0ad79
masakihigashi <masaki.h.5555@gmail.com>
2021-04-22 23:13:06 +0900

    Add more unit tests for controllers

---
+++ src/test/java/com/msk/taskmanager/controller/RegisterControllerTest.java
@@ -0,0 +1,49 @@
+package com.msk.taskmanager.controller;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.msk.taskmanager.model.User;
+import com.msk.taskmanager.service.UserService;
+
+public class RegisterControllerTest {
+
+    @Mock
+    UserService userServiceMock;
+
+    @InjectMocks
+    RegisterController registerController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(registerController).build();
+    }
+
+    @Test
+    public void showRegisterForm_shouldReturnStatusOkAndRegisterFormAsViewNameAndUserAsAttribute() throws Exception {
+        Long id = 1L;
+
+        verifyZeroInteractions(userServiceMock);
+
+        mockMvc.perform(get("/register"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("views/registerForm"))
+                .andExpect(model().attribute("user", instanceOf(User.class)));
+    }
+
+}
---
+++ src/test/java/com/msk/taskmanager/controller/TaskControllerTest.java
@@ -0,0 +1,63 @@
+package com.msk.taskmanager.controller;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.msk.taskmanager.model.Task;
+import com.msk.taskmanager.service.TaskService;
+
+public class TaskControllerTest {
+
+    @Mock
+    TaskService taskServiceMock;
+
+    @InjectMocks
+    TaskController taskController;
+    private MockMvc mockMvc;
+
+    @Before
+    @WithMockUser(username = "admin", authorities = { "ADMIN" })
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(taskController).build();
+    }
+
+    @Test
+    public void updateTask_shouldReturnStatusOkAndFilledTaskFormAsViewNameAndTaskAsAttribute() throws Exception {
+        Long id = 1L;
+
+        when(taskServiceMock.getTaskById(id)).thenReturn(new Task());
+
+        mockMvc.perform(get("/task/edit/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("views/filledTaskForm"))
+                .andExpect(model().attribute("task", instanceOf(Task.class)));
+    }
+
+    @Test
+    public void deleteTask_shouldInvokeDeleteTaskMethodAndRedirectToTasks() throws Exception {
+        Long id = 1L;
+
+        mockMvc.perform(get("/task/delete/1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/tasks"));
+
+        verify(taskServiceMock, times(1)).deleteTask(id);
+    }
+
+}
